410716a0092f8235f5bad9462d378295dda895a5
qmarkhow-spec <qmarkhow@gmail.com>
2026-01-10 00:52:42 +0800

    1252

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
+  output: "standalone",
+  outputFileTracingRoot: path.join(__dirname, ".."),
   eslint: {
     ignoreDuringBuilds: true
   }
